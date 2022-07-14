0af6fcbef0ab9c5522b0aa4e80bc970bea5f3599
Nahidujjaman Hridoy <75487507+nhridoy@users.noreply.github.com>
2022-07-14 16:19:46 +0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -96,7 +96,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: 'corporate',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
